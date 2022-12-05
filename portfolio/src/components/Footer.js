ae10b46f2f495099c424a62200f37c0fb7038b6c
A.Klein <allenkleined@gmail.com>
2022-12-04 19:37:15 -0800

    added links to footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            This is a footer
+           <a href="https://github.com/allen-ek">Github</a>
+           <a href="https://www.linkedin.com/in/allen-klein-506031251/">LinkedIn</a>
+
         </footer>
     )
 }
