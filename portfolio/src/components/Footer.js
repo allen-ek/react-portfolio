d335fce73082e1857ede77e7e26c8372c6550054
A.Klein <allenkleined@gmail.com>
2022-12-04 19:56:58 -0800

    added third link to footer and image to page

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,9 +2,10 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-           <a href="https://github.com/allen-ek">Github</a>
-           <a href="https://www.linkedin.com/in/allen-klein-506031251/">LinkedIn</a>
-
+           <a href="https://github.com/allen-ek" target="_blank" rel="noopener noreferrer">Github</a>
+           <a href="https://www.linkedin.com/in/allen-klein-506031251/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+           <a href="https://medium.com/@allenkleined" target="_blank" rel="noopener noreferrer">Medium</a>
+           
         </footer>
     )
 }
--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Avatar from'../../avatar.png'
 
 export default function About() {
   return (
     <div>
       <h1>About me</h1>
+      <img src={Avatar} alt="profile" />
       <p>
-        
+      Engineering undergraduate from University of California Santa Cruz, majoring in Technology Information Management. 
+      Multifaceted knowledge in computer science, business, financial processes with a focus on software implementation and technological support.
       </p>
     </div>
   );
